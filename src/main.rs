af096a1b101f821e1942f3487d0fd08f568a45dc
Matth-M <matthieu.matsima@gmail.com>
2025-01-27 15:37:21 +0100

    Add spheres

--- src/main.rs
+++ src/main.rs
@@ -12,25 +12,55 @@ fn main() {
         material_type: MaterialType::Lambertian,
         albedo: Color::from([0.5, 0.5, 0.5]),
     });
+    let material_center = Rc::new(Material {
+        material_type: MaterialType::Lambertian,
+        albedo: Color::from([0.1, 0.2, 0.5]),
+    });
+    let material_left = Rc::new(Material {
+        material_type: MaterialType::Metal,
+        albedo: Color::from([0.8, 0.8, 0.8]),
+    });
+    let material_right = Rc::new(Material {
+        material_type: MaterialType::Metal,
+        albedo: Color::from([0.8, 0.6, 0.2]),
+    });
 
     let objects = vec![
         Rc::new(Sphere {
             center: Point {
-                x: 1.,
-                y: 0.,
-                z: 0.,
-            },
-            radius: 0.5,
-            material: Rc::clone(&material_ground),
-        }),
-        Rc::new(Sphere {
-            center: Point {
-                x: 1.,
+                x: 0.,
                 y: -100.5,
                 z: 0.,
             },
             radius: 100.,
             material: Rc::clone(&material_ground),
+        }),
+        Rc::new(Sphere {
+            center: Point {
+                x: 1.2,
+                y: 0.,
+                z: 0.,
+            },
+            radius: 0.5,
+            material: Rc::clone(&material_center),
+        }),
+        Rc::new(Sphere {
+            center: Point {
+                x: 1.2,
+                y: 0.,
+                z: -1.,
+            },
+            radius: 0.5,
+            material: Rc::clone(&material_left),
+        }),
+        Rc::new(Sphere {
+            center: Point {
+                x: 1.2,
+                y: 0.,
+                z: 1.,
+            },
+            radius: 0.5,
+            material: Rc::clone(&material_right),
         }),
     ];
 
